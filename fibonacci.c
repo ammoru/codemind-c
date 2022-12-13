7f6e9b0758b31b94dd0b19ccbe96b5969ceff25a
ammoru <119839681+ammoru@users.noreply.github.com>
2022-12-13 10:15:57 +0530

    #include<stdio.h> int main() {     int i,a,b,n,c;     scanf("%d",&n);     a=0;     b=1;     for(i=0;i<n;i++)     {         printf("%d ",a);         c=a+b;         a=b;         b=c;     } }

---
+++ fibonacci.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,a,b,n,c;
+    scanf("%d",&n);
+    a=0;
+    b=1;
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a);
+        c=a+b;
+        a=b;
+        b=c;
+    }
+}
