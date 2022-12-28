65bded8950639bda7cc87862ff8684cb2b819c73
ammoru <119839681+ammoru@users.noreply.github.com>
2022-12-28 10:26:28 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)    {        scanf("%d",&a[i]);    }        for(i=0;i<n;i++)     {         if(i%2==0)         {         if(a[i]%2!=0)         c++;         }     }   if(c>=1)   printf("False");   else   printf("True"); }

---
+++ Strictly_ODD.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+       for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+        if(a[i]%2!=0)
+        c++;
+        }  
+    }
+  if(c>=1)
+  printf("False");
+  else
+  printf("True");
+}
