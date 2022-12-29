a2d3449a0ff15ae55dc6a9dc3798a71300e56042
ammoru <119839681+ammoru@users.noreply.github.com>
2022-12-29 21:38:05 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,k1=0,k2=0,a,b;     scanf("%d",&n);     printf("0 ");     for(i=0;i<(n-1);i++)      {          if(i%2==0)          {              a=pow(3,k1);              printf("%d ",a);              k1++;          }else          {              b=pow(2,k2);              printf("%d ",b);              k2++;          }      } }

---
+++ series.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main() 
+{
+    int n,i,k1=0,k2=0,a,b;
+    scanf("%d",&n);
+    printf("0 ");
+    for(i=0;i<(n-1);i++)
+     {
+         if(i%2==0)
+         {
+             a=pow(3,k1);
+             printf("%d ",a);
+             k1++;
+         }else
+         {
+             b=pow(2,k2);
+             printf("%d ",b);
+             k2++;
+         }
+     }
+}
